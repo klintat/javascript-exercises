7889dbe6e452aaeed480f455257a2e4bfd5d86d7
klintat <98770096+klintat@users.noreply.github.com>
2023-12-12 23:51:43 +0200

    Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // let splitString = string.split("");
+    // let reverseArray = splitString.reverse();
+    // let joinArray = reverseArray.join("");
+    // if(joinArray === string) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
 
+    let processedStr = string.toLowerCase().replace(re, "");
+    return processedStr == processedStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
